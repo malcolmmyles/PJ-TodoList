7dd77cce7c8834353679f233c06fa7d3e933c96f
malcolmmyles <malcolmmyles@live.com>
2016-06-20 18:59:19 -0700

    9-20

--- js/script.js
+++ js/script.js
@@ -63,3 +63,15 @@ var todoList = {
     this.displayTodos();
   }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll()
+  
+});
